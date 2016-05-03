dfdca1a7945c732dac8558fdfb7c7d5e5a1a319e
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2016-05-03 18:20:44 +0900

    add NewWrapper, NewWrapperWith

--- example_test.go
+++ example_test.go
@@ -7,7 +7,7 @@ import (
 	"github.com/mattn/sorter"
 )
 
-func ExampleSorter() {
+func ExampleWrapper() {
 	s := []string{
 		"2",
 		"3",
@@ -25,6 +25,57 @@ func ExampleSorter() {
 			s[i], s[j] = s[j], s[i]
 		},
 	})
+
+	for _, v := range s {
+		fmt.Println(v)
+	}
+	// Output:
+	// 1
+	// 2
+	// 3
+}
+
+func ExampleNewWrapperWith() {
+	s := []string{
+		"2",
+		"3",
+		"1",
+	}
+
+	sort.Sort(sorter.NewWrapperWith(
+		s,
+		func(i, j int) bool {
+			return s[i] < s[j]
+		},
+	))
+
+	for _, v := range s {
+		fmt.Println(v)
+	}
+	// Output:
+	// 1
+	// 2
+	// 3
+}
+
+func ExampleNewWrapper() {
+	s := []string{
+		"2",
+		"3",
+		"1",
+	}
+
+	sort.Sort(sorter.NewWrapper(
+		func() int {
+			return len(s)
+		},
+		func(i, j int) bool {
+			return s[i] < s[j]
+		},
+		func(i, j int) {
+			s[i], s[j] = s[j], s[i]
+		},
+	))
 
 	for _, v := range s {
 		fmt.Println(v)
--- sorter.go
+++ sorter.go
@@ -1,9 +1,20 @@
 package sorter
 
+import (
+	"reflect"
+	"sort"
+)
+
+type (
+	FLen  func() int
+	FLess func(i, j int) bool
+	FSwap func(i, j int)
+)
+
 type Wrapper struct {
-	LenFunc  func() int
-	LessFunc func(i, j int) bool
-	SwapFunc func(i, j int)
+	LenFunc  FLen
+	LessFunc FLess
+	SwapFunc FSwap
 }
 
 func (w *Wrapper) Len() int {
@@ -16,4 +27,39 @@ func (w *Wrapper) Less(i, j int) bool {
 
 func (w *Wrapper) Swap(i, j int) {
 	w.SwapFunc(i, j)
+}
+
+func NewWrapper(fLen FLen, fLess FLess, fSwap FSwap) sort.Interface {
+	return &Wrapper{
+		LenFunc:  fLen,
+		LessFunc: fLess,
+		SwapFunc: fSwap,
+	}
+}
+
+type WrapperWith struct {
+	Value    reflect.Value
+	LessFunc FLess
+}
+
+func (w *WrapperWith) Len() int {
+	return w.Value.Len()
+}
+
+func (w *WrapperWith) Less(i, j int) bool {
+	return w.LessFunc(i, j)
+}
+
+func (w *WrapperWith) Swap(i, j int) {
+	v1 := w.Value.Index(i).Interface()
+	v2 := w.Value.Index(j).Interface()
+	w.Value.Index(j).Set(reflect.ValueOf(v1))
+	w.Value.Index(i).Set(reflect.ValueOf(v2))
+}
+
+func NewWrapperWith(arr interface{}, fLess FLess) sort.Interface {
+	return &WrapperWith{
+		Value:    reflect.ValueOf(arr),
+		LessFunc: fLess,
+	}
 }
